78a67b2b34356187eba80291abfc3717899ae09c
edsoalter <edsolater@qq.com>
2022-08-24 23:06:26 +0800

    🎨

--- src/tempUikits/Button.tsx
+++ src/tempUikits/Button.tsx
@@ -1,12 +1,16 @@
 import { Button as _Button, ButtonHandle as _ButtonHandle, ButtonProps as _ButtonProps } from '@edsolater/uikit'
 import LoadingCircleSmall from './LoadingCircleSmall'
 
-export interface ButtonHandle extends _ButtonHandle {}
+/**********************************************************
+ * DONE have extends uikit's <Button>     *
+ **********************************************************/
 export interface ButtonProps extends Omit<_ButtonProps, 'variant'> {
   type?: 'solid' | 'outline' | 'text'
   /** unused tailwind style class string will be tree-shaked  */
   isLoading?: boolean
 }
+
+export type ButtonHandle = _ButtonHandle
 
 /** has loaded **twMerge** */
 export default function Button({ type, isLoading, ...restButtonProps }: ButtonProps) {
@@ -26,65 +30,4 @@ export default function Button({ type, isLoading, ...restButtonProps }: ButtonPr
       }
     />
   )
-}
-
-/** base inner <Button> style  */
-function solidButtonTailwind({
-  size = 'default',
-  disable,
-  haveFallbackClick
-}: { size?: 'xs' | 'md' | 'sm' | 'lg' | 'default'; disable?: boolean; haveFallbackClick?: boolean } = {}) {
-  return `${
-    size === 'lg'
-      ? 'px-6 py-3.5 rounded-xl font-bold'
-      : size === 'sm'
-      ? 'px-4 py-2 text-sm rounded-xl font-medium'
-      : size === 'xs'
-      ? 'px-4 py-2 text-xs rounded-xl font-medium'
-      : 'px-4 py-2.5  rounded-xl font-medium'
-  } whitespace-nowrap appearance-none inline-block ${
-    disable
-      ? `bg-formkit-thumb-disable text-formkit-thumb-text-disabled opacity-40 ${
-          haveFallbackClick ? '' : 'cursor-not-allowed'
-        }`
-      : 'bg-formkit-thumb text-formkit-thumb-text-normal clickable clickable-filter-effect'
-  }`
-}
-
-/** extra inner <Button> style */
-function outlineButtonTailwind({
-  size = 'default',
-  disable,
-  haveFallbackClick
-}: { size?: 'xs' | 'md' | 'sm' | 'lg' | 'default'; disable?: boolean; haveFallbackClick?: boolean } = {}) {
-  return `${
-    size === 'lg'
-      ? 'py-4 px-4 rounded-xl'
-      : size === 'sm'
-      ? 'px-2.5 py-1.5 text-sm rounded-xl'
-      : size === 'xs'
-      ? 'px-4 py-2 text-xs rounded-xl'
-      : 'px-4 py-2.5  rounded-xl'
-  } whitespace-nowrap appearance-none inline-block ring-1.5 ring-inset ring-current ${
-    disable ? `opacity-40 ${haveFallbackClick ? '' : 'cursor-not-allowed'}` : 'clickable clickable-filter-effect'
-  }`
-}
-
-/** extra inner <Button> style */
-function textButtonTailwind({
-  size = 'default',
-  disable,
-  haveFallbackClick
-}: { size?: 'xs' | 'md' | 'sm' | 'lg' | 'default'; disable?: boolean; haveFallbackClick?: boolean } = {}) {
-  return `${
-    size === 'lg'
-      ? 'py-4 px-4 rounded-xl'
-      : size === 'sm'
-      ? 'px-2.5 py-1.5 text-sm rounded-xl'
-      : size === 'xs'
-      ? 'px-4 py-2 text-xs rounded-xl'
-      : 'px-4 py-2.5  rounded-xl'
-  } whitespace-nowrap appearance-none inline-block text-white ${
-    disable ? `opacity-40 ${haveFallbackClick ? '' : 'cursor-not-allowed'}` : 'clickable clickable-filter-effect'
-  }`
 }
--- src/tempUikits/FadeIn.tsx
+++ src/tempUikits/FadeIn.tsx
@@ -2,6 +2,5 @@ import { FadeIn as _FadeIn } from '@edsolater/uikit'
 
 const FadeInStable = _FadeIn
 export default FadeInStable
-
 /** trans width/height from zero to auto */
 export const FadeIn = _FadeIn
