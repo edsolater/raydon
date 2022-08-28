54e25c883b6740867a30e275800b73b46fb10bee
edsoalter <edsolater@qq.com>
2022-08-28 12:45:29 +0800

    🚧 use uikit `<Dialog>` and `<Drawer>`

--- src/tempUikits/Drawer.tsx
+++ src/tempUikits/Drawer.tsx
@@ -1,15 +1,13 @@
 import React, { CSSProperties, Fragment, ReactNode, useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 
-import { Transition } from '@headlessui/react'
-
 import { twMerge } from 'tailwind-merge'
 
 import { inClient } from '@/functions/judgers/isSSR'
 import { shrinkToValue } from '@/functions/shrinkToValue'
 import useTwoStateSyncer from '@/hooks/use2StateSyncer'
 import { MayFunction } from '@/types/constants'
-import { Div, DivProps } from '@edsolater/uikit'
+import { Div, DivProps, Portal, Transition } from '@edsolater/uikit'
 
 export const DRAWER_STACK_ID = 'drawer-stack'
 
@@ -32,15 +30,14 @@ const placementClasses = {
   }
 }
 
-export interface DrawerProps {
-  className?: string
-  style?: CSSProperties
+export interface DrawerProps extends Omit<DivProps, 'children'> {
   children?: MayFunction<ReactNode, [{ close(): void }]>
   open: boolean
   placement?: 'from-left' | 'from-bottom' | 'from-top' | 'from-right'
   transitionSpeed?: 'fast' | 'normal'
   // if content is scrollable, PLEASE open it!!!, for blur will make scroll super fuzzy
   maskNoBlur?: boolean
+  canClosedByMask?: boolean
   onOpen?: () => void
   /** fired before close transform effect is end */
   onCloseImmediately?: () => void
@@ -68,11 +65,13 @@ export default function Drawer({
   placement = 'from-left',
   transitionSpeed = 'normal',
   maskNoBlur,
+  canClosedByMask = true,
   onOpen,
   onCloseImmediately,
-  onClose
+  onClose,
+  ...divProps
 }: DrawerProps) {
-  const drawerContentRef = useRef<HTMLDivElement>(null)
+  const transitionDuration = transitionSpeed === 'fast' ? 200 : 300
 
   // for onCloseTransitionEnd
   // during leave transition, open is still true, but innerOpen is false, so transaction will happen without props:open has change (if open is false, React may destory this component immediately)
@@ -94,44 +93,61 @@ export default function Drawer({
   })
 
   return (
-    <DrawerStackPortal>
-      <Transition as={Fragment} appear show={innerOpen} beforeLeave={onCloseImmediately} afterLeave={onClose}>
-        <div className="Drawer w-full h-full fixed" onClick={(ev) => ev.stopPropagation()}>
-          <Transition.Child
-            as={Fragment}
-            enter={`ease-out ${transitionSpeed === 'fast' ? 'duration-150' : 'duration-300'} transition`}
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave={`ease-in ${transitionSpeed === 'fast' ? 'duration-100' : 'duration-200'} transition`}
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div
-              className={`absolute inset-0 ${
-                maskNoBlur ? '' : 'backdrop-filter backdrop-blur'
-              } bg-[rgba(25,19,88,0.5)]`}
-              onClick={closeDrawer}
-            ></div>
-          </Transition.Child>
-          <Transition.Child
-            as={Fragment}
-            enter={`ease-out ${transitionSpeed === 'fast' ? 'duration-150' : 'duration-300'} transform transition`}
-            enterFrom={`${placementClasses[placement].translateFadeOut}`}
-            enterTo=""
-            leave={`ease-out ${transitionSpeed === 'fast' ? 'duration-150' : 'duration-300'} transform transition`}
-            leaveFrom=""
-            leaveTo={`${placementClasses[placement].translateFadeOut}`}
-          >
-            <div
-              className={twMerge(`absolute ${placementClasses[placement].absolutePostion}`, className)}
-              style={style}
-              ref={drawerContentRef}
-            >
-              {shrinkToValue(children, [{ open: openDrawer, close: closeDrawer }])}
-            </div>
-          </Transition.Child>
-        </div>
+    <Portal
+      id={DRAWER_STACK_ID}
+      zIndex={1010}
+      icss={{
+        position: 'fixed',
+        inset: 0,
+        pointerEvents: 'none',
+        '*': {
+          pointerEvents: 'initial'
+        }
+      }}
+      onClick={({ ev }) => ev.stopPropagation()}
+    >
+      <Transition
+        show={innerOpen}
+        fromProps={{ icss: { opacity: 0 } }}
+        toProps={{ icss: { opacity: 1 } }}
+        cssTransitionDurationMs={75}
+        onBeforeLeave={onCloseImmediately}
+        onAfterLeave={onClose}
+        icss={{ transitionDelay: innerOpen ? `${transitionDuration}ms` : '', position: 'fixed', inset: 0 }}
+      >
+        <Div
+          className={'Drawer-mask'}
+          icss={{
+            background: '#0000005c',
+            backdropFilter: maskNoBlur ? undefined : 'blur(10px)',
+            pointerEvents: canClosedByMask ? undefined : 'none'
+          }}
+          onClick={closeDrawer}
+        />
       </Transition>
-    </DrawerStackPortal>
+
+      <Transition
+        className={'Drawer-content'}
+        show={innerOpen}
+        fromProps={{ className: placementClasses[placement].translateFadeOut }}
+        toProps={{ className: '' }}
+        cssTransitionDurationMs={transitionDuration}
+      >
+        <Div
+          {...divProps}
+          icss={[
+            {
+              position: 'fixed',
+              inset: 0,
+              pointerEvents: 'none',
+              '*': { pointerEvents: 'initial' }
+            },
+            divProps.icss
+          ]}
+        >
+          {shrinkToValue(children, [{ close: closeDrawer, open: openDrawer }])}
+        </Div>
+      </Transition>
+    </Portal>
   )
 }
